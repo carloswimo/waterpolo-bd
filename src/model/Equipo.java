1e69deb99756a91aa1d9d09b326bbcff46f4cebf
carlos <carlos@carlos-Think>
2018-06-04 22:39:30 +0200

    equipo pasado a limpio

--- src/model/Equipo.java
+++ src/model/Equipo.java
@@ -1,9 +1,5 @@
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model;
 
 import java.util.*;
@@ -72,52 +68,129 @@ public class Equipo {
     public void setPais(String pais) {
         this.pais = pais;
     }
+    
+    
     // --------- OPERACIONES BD ----------------------------------------
 
     // ---------- CRUD BÁSICO
     public boolean create() {
-        // new objeto vacio
-        // relleno todos los datos menos el id
-        // invoco cuando quiera guardar
-        // relleno desde interfaz usuario
-        //insert
-        return true;
+        // new objeto vacio, relleno todos los datos menos el id, invoco cuando quiera guardar, relleno desde interfaz usuario
+        // insert
+        boolean todoOk = true;
+        
+        try (Connection conn = ConexionBd.obtener()) {
+            try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO equipo (nombre, ciudad, pais) VALUES (?, ?, ?)")) {
+                
+                stmt.setString(1, getNombre());
+                stmt.setString(2, getCiudad());
+                stmt.setString(3, getPais());
+
+                stmt.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            todoOk = false;
+        }
+        return todoOk;
     }
 
     public boolean retrieve() {
-        // POR HACER
-        setId(33);
-        setNombre("Equipo ejemplo");
-        setCiudad("Ciudad ejemplo");
-        setPais("Pais ejemplo");
-        return true;
+        boolean todoOk = true;
         
-        // tener un objeto tener el id, y que le resto de lso campos se rellenen
-        // es un select por id
-        // 
+        try (Connection conn = ConexionBd.obtener()) {
+            
+            try (PreparedStatement stmt = conn.prepareStatement("SELECT nombre, ciudad, pais FROM equipo WHERE id = ?")) {
+                
+                stmt.setInt(1, getId());
+
+                try (ResultSet rs = stmt.executeQuery()) {
+                    
+                    while (rs.next()) {
+                        setNombre(rs.getString("nombre"));
+                        setCiudad(rs.getString("ciudad"));
+                        setPais(rs.getString("pais"));
+                    }
+                    
+                }
+            }
+        } catch (SQLException ex) {
+            todoOk = false;
+        }
+
+        return todoOk;
+        
+        // tener un objeto tener el id, y que le resto de lso campos se rellenen, es un select por id
+        
     }
 
     public boolean update() {
-        return true;
-        // previamente tiene que haber un retrieve
-        // hay un objeto relleno
+        
+        // previamente tiene que haber un retrieve, hay un objeto relleno
         // se cambia lo que sea y se hace un update de todos los campos menos del id
+        
+        boolean todoOk = true;
+        
+        try (Connection conn = ConexionBd.obtener()) {
+            try (PreparedStatement stmt = conn.prepareStatement("UPDATE equipo SET nombre = ?, ciudad = ?, pais = ? WHERE id = ?")) {
+                stmt.setString(1, getNombre());
+                stmt.setString(2, getCiudad());
+                stmt.setString(3, getPais());
+                stmt.setInt(4, getId());
+
+                stmt.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            todoOk = false;
+        }
+        return todoOk;
     }
 
     public boolean delete() {
-        // creo un objeto
-        // relleno el id
-        // delete con lo que haya en el campod el id
-        return true;
+        // creo un objeto, relleno el id, delete con lo que haya en el campod el id
+        boolean todoOk = true;
+        
+        try (Connection conn = ConexionBd.obtener()) {
+            
+            try (PreparedStatement stmt = conn.prepareStatement("DELETE FROM equipo WHERE id = ?")) {
+                stmt.setInt(1, getId());
+                stmt.executeUpdate();
+            }
+            
+        } catch (SQLException ex) {
+            todoOk = false;
+        }
+        
+        return todoOk;
     }
 
     // ----------- Otras, de instancia, relacionadas con la fk
     public List<Jugador> getJugadores() {
-        // POR HACER.
+
+        
+        
         List<Jugador> resultado = new ArrayList<>();
-        resultado.add(new Jugador(1, "Paco", "López", 19));
-        resultado.add(new Jugador(2, "Luisa", "Martínez", 21));
+        
+        boolean todoOk = true;
+        
+        try (Connection conn = ConexionBd.obtener()) {
+            
+            try (PreparedStatement stmt = conn.prepareStatement("SELECT nombre, apellidos, edad FROM jugador WHERE idequipo = ?")) {
+                
+                stmt.setInt(1, getId());
+
+                try (ResultSet rs = stmt.executeQuery()) {
+                    
+                    while (rs.next()) {                      
+                        resultado.add(new Jugador(rs.getString("nombre"), rs.getString("apellidos"), rs.getInt("edad"), getId()));
+                    }
+                }
+                
+            }
+        } catch (SQLException ex) {
+            todoOk = false;
+        }
+        
         return resultado;
+        
     }
 
     // ----------- Otras, de clase, no relacionadas con ÉSTE (this) objeto
@@ -126,16 +199,55 @@ public class Equipo {
         if (!(orden >= 0 && orden <= 1)) {
             throw new IllegalArgumentException("Parámetro de orden de equipos no admitido");
         }
+        
+        List<Equipo> resultado = null;
+        boolean todoOk = true;
+        
+        try (Connection conn = ConexionBd.obtener()) {
+            resultado = new ArrayList<>();
+            String sql = "SELECT id, nombre, ciudad, pais FROM EQUIPO";
+            
+            if (!(busqueda.equals(""))) {
+                
+                sql = sql + " WHERE LOWER(nombre) LIKE ? OR LOWER(ciudad) LIKE ? OR LOWER(pais) LIKE ?";
+                sql = orden == 0 ? sql + " ORDER BY nombre" : sql + " ORDER BY pais";
+                
+                try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                    String busquedaSql = "%" + busqueda.toLowerCase() + "%";
+                    stmt.setString(1, busquedaSql);
+                    stmt.setString(2, busquedaSql);
+                    stmt.setString(3, busquedaSql);
+                    try (ResultSet rs = stmt.executeQuery()) {
+                        
+                        while (rs.next()) {
+                            resultado.add(new Equipo(rs.getInt("id"), rs.getString("nombre"),
+                                    rs.getString("ciudad"), rs.getString("pais")));
+                        }
+                    }
+                }
+            } else {
+                sql = orden == 0 ? sql + " ORDER BY nombre" : sql + " ORDER BY pais";
+                
+                try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                    
+                    try (ResultSet rs = stmt.executeQuery()) {
+                        
+                        while (rs.next()) {
+                            resultado.add(new Equipo(rs.getInt("id"), rs.getString("nombre"),
+                                    rs.getString("ciudad"), rs.getString("pais")));
+                        }
+                    }
+                }
+            }
+        } catch (SQLException ex) {
+            todoOk = false;
+        }
 
-        // Si la búsqueda es una cadena vacía lanzamos una select sin WHERE
-        // y si tiene algo con WHERE y varios LIKEs
-        // POR HACER
-        List<Equipo> resultado = new ArrayList<>();
-        resultado.add(
-                new Equipo(1, "Halcones calvos", "Getafe", "España"));
-        resultado.add(
-                new Equipo(2, "Dumma den som läser den", "Visby", "Suecia"));
-        return resultado;
+        if (!todoOk) {
+            return resultado;
+        } else {
+            return null;
+        }
 
     }
 
